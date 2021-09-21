87458c0ca029cf91f252450bd457c401bd5c7c51
Chris Keesey <c2keesey@gmail.com>
2021-09-21 14:47:16 -0700

    calloc no work

--- test_main.ino
+++ test_main.ino
@@ -4,15 +4,14 @@
 #define LED_PIN 2
 #define DIAL 25
 
-CRGB *leds;
+CRGB LEDs[NUM] = {0};
 
 uint8_t hue = 0;
 uint8_t mode = 1;
 
 void setup() {
-  leds = calloc(NUM, sizeof(CRGB));
   pinMode(LED_PIN, OUTPUT);
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM);
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(LEDs, NUM);
   FastLED.setBrightness(100);
   FastLED.clear();
   FastLED.setMaxPowerInMilliWatts(5000);
@@ -22,7 +21,7 @@ void loop() {
   switch(mode) {
     case 0:
       for (int i = 0; i < NUM; i++) {
-        leds[i] = CHSV(hue + i, 255, 255);
+        LEDs[i] = CHSV(hue + i, 255, 255);
       }
     
       EVERY_N_MILLIS(15) {
@@ -31,12 +30,12 @@ void loop() {
       }
     case 1:
       EVERY_N_MILLIS(15) {
-        leds[random(NUM)] = CHSV(random(185, 250), random(80, 100), 255);
+        LEDs[random(NUM)] = CHSV(random(185, 250), random(80, 100), 255);
         FastLED.show();
       }
       EVERY_N_MILLIS(50) {
         for(int i = 0; i < NUM; i++) {
-          fadeToBlackBy(leds, NUM, 10);
+          fadeToBlackBy(LEDs, NUM, 10);
         }
         FastLED.show();
       }
